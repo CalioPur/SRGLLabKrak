1a078acd8b8d0c376ac6a2410dc7a6dd7a2656ef
pp <a.armar2003@fmail.com>
2022-04-28 11:11:17 +0200

    meniqsue ajouté

---
+++ SRGL Labs/Assets/Scripts/MiniGamePropipette/comprtementMenisque.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comprtementMenisque : MonoBehaviour
+{
+    public Material matFill;
+    public float offset;
+    //public Material monMat;
+    //Color monMatColor;
+    private void Update()
+    { 
+        transform.position = new Vector3(0, (4*matFill.GetFloat("_fill"))+offset, 0);
+    }
+}
